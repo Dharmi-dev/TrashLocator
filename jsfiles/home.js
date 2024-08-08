91a2e1ea128215e93a2183ed5e4debb49475db35
Dharmi-dev <dharmesh230335@keshav.du.ac.in>
2024-08-08 22:06:29 +0530

    added preloader

---
+++ jsfiles/home.js
@@ -0,0 +1,15 @@
+
+
+
+// preloader 
+
+const preloader = document.querySelector("#preloader")
+
+function Mypreloader(){
+  setTimeout(() =>{
+    preloader.style.display = "none";
+  },1000)
+}
+
+window.addEventListener("load", Mypreloader);
+
