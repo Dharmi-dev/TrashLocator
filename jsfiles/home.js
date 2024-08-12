b236ff709e7dc8bf664e744d379110b3b4960f42
Dharmi-dev <dharmesh230335@keshav.du.ac.in>
2024-08-12 15:24:43 +0530

    completely added the hamburger menu functionality

--- jsfiles/home.js
+++ jsfiles/home.js
@@ -2,14 +2,38 @@
 
 const menuBtn = document.querySelector(".ri-menu-line");
 const sideBar = document.querySelector(".side-navbar");
-menuBtn.addEventListener("click", showSidebar)
+const crossBtn = document.querySelector(".cross-btn-nav");
+
+
+
+
 
 
 function showSidebar(){
-  if(sideBar.style.display === "none"){
+  console.log("btn clicked");
+  if(sideBar.style.display == "none"){
     sideBar.style.display = "flex";
-  }else sideBar.style.display = "none";
+    console.log("display changed to flex")
+  }
+  // else {
+  //   sideBar.style.display = "none";
+  //   console.log("display changed to none")
+  // }
 }
+
+function hideSidebar(){
+  console.log("inside hidebar")
+  // if(sideBar.style.display == "flex"){
+    
+  //   console.log("sidebar is hidden")
+  // }
+  sideBar.style.display = "none";
+}
+
+
+crossBtn.addEventListener("click", hideSidebar)
+menuBtn.addEventListener("click", showSidebar)
+
 
 
 // preloader 
